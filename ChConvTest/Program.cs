2620239eb259ecf2d3727399c794865101d6868f
Jeffrey Lee <darkthread@gmail.com>
2022-03-06 12:15:05 +0800

    PoC

---
+++ ChConvTest/Program.cs
@@ -0,0 +1,7 @@
+﻿var s = "預設記憶體大小與硬碟容量";
+var resPath = 
+    Path.GetDirectoryName(typeof(Program).Assembly.Location);
+var c = new ChConvUtil.Converter(resPath);
+Console.WriteLine(c.TWtoCN(s));
+Console.WriteLine(c.CNtoTW(s));
+
---
+++ ChConvUtil/Converter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using OpenCCNET;
+
+namespace ChConvUtil
+{
+    public class Converter
+    {
+        public Converter(string path = ".") {
+            var jiebaResourceDirectory = Path.Combine(path, "JiebaResource");
+            JiebaNet.Segmenter.ConfigManager.ConfigFileBaseDir = jiebaResourceDirectory;
+            ZhConverter.Initialize();
+            ZhDictionary.Initialize(
+                Path.Combine(path, "Dictionary"));
+        }
+        public string TWtoCN(string src) => ZhConverter.TWToHans(src, true);
+        public string CNtoTW(string src) => ZhConverter.HansToTW(src, true);
+    }
+}
